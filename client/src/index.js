1ec31143789a35ecaab825913d015241b4cf70ff
Tropik42 <aviavova94@gmail.com>
2021-11-28 15:38:47 +0300

    Header added

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import {store} from './reducers'
-import {Provider} from 'react-redux'
+import { render } from 'react-dom';
+import { compose, createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import App from './App.jsx';
+import { rootReducer } from './redux/rootReducer.js';
 
-ReactDOM.render(
-  <Provider store = {store}>
-    <App />
-  </Provider>,  
-  document.getElementById('root')
+const store = createStore(rootReducer, compose(
+  applyMiddleware(
+      thunk
+  ),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+const app = (
+  <Provider store={store}>
+      <App />
+  </Provider>
 );
 
+render(app, document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
---
+++ client/src/redux/headerReducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+  posts: [],
+  fetchedPosts: []
+}
+
+// Pure Functions
+export const headerReducer = (state = initialState, action) => {
+    return state
+}

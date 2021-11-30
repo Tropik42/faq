9d3b0d5e96351e3f2f8dc034608c8e23a6bb38f5
Tropik42 <aviavova94@gmail.com>
2021-11-30 21:41:49 +0300

    cards added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Main from "./components/Main";
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-xl-2">
+            <Sidebar />
+          </div>
+          <div className="col-xl-10">
+            <Main />
+          </div>                 
+        </div>        
+      </div>      
+    </>
   );
 }
 
---
+++ client/src/components/Main.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+function Main () {
+    return (
+        <div className="main">
+<div class="row row-cols-1 row-cols-md-2 g-4 m-5">
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Название карточки</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Название карточки</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Название карточки</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Название карточки</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+        </div>
+    )
+}
+
+export default Main;

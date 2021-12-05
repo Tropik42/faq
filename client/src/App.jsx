f69841a16f95fd4e5db1aff6a5cf84d7263c0fe0
Tropik42 <aviavova94@gmail.com>
2021-12-05 22:33:25 +0300

    Cards refactored

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
+import React from 'react';
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
-import Main from "./components/Main";
+import Main from "./components/Main/Main";
+import './App.css'
 
 function App() {
   return (
@@ -8,10 +10,10 @@ function App() {
       <Header />
       <div className="container-fluid">
         <div className="row">
-          <div className="col-xl-2">
+          <div className="col-xl-2 p-0">
             <Sidebar />
           </div>
-          <div className="col-xl-10">
+          <div className="col-xl-10 pt-5 main">
             <Main />
           </div>                 
         </div>        
